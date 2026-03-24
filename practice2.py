ead059f4983c7b66b38031e18e71b5dd1f25ac53
문승욱 <uoowoq@gmail.com>
2026-03-24 12:22:24 +0900

    python P01

---
+++ practice2.py
@@ -0,0 +1,22 @@
+lang ='PYTHON'
+Print(lang)
+print(lanf[0])
+print(lang[-1])
+print(lang[:3])
+print(lang[2:])
+print([:])
+
+num = 3
+num += 2# num =num+2
+print(num)
+
+num -=1
+num *=2
+num /=4
+print(num)
+
+
+snack = '꿀꽈베기'
+print(len(snack))
+
+
